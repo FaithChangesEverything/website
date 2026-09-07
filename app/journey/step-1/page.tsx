3e4b8ad98a835a16fc53e1c95349c504a551dd14
Richard Ball <richiebee47@gmail.com>
2026-09-06 22:03:51 -0500

    Remove redundant Step 1 route override

--- app/journey/step-1/page.tsx
+++
@@ -1,6 +0,0 @@
-import { StepOverview } from "../components/PageStructures";
-import { journeySteps } from "../data";
-
-export default function StepOnePage() {
-  return <StepOverview stepNumber={1} lessons={journeySteps[0].lessons} />;
-}
