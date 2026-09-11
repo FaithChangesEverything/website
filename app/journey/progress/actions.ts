bcc3f1992978adb35162aaaf07f61319e21c34b0
Richard Ball <richiebee47@gmail.com>
2026-09-11 18:08:36 -0500

    Make J2H form server actions return void

--- app/journey/progress/actions.ts
+++ app/journey/progress/actions.ts
@@ -3,14 +3,12 @@
 import { revalidatePath } from "next/cache";
 import { completeJourneyItem, reverseJourneyItemCompletion } from "./operations";
 
-export async function completeJourneyItemAction(contentKey: string, path: string) {
-  const result = await completeJourneyItem(contentKey);
+export async function completeJourneyItemAction(contentKey: string, path: string): Promise<void> {
+  await completeJourneyItem(contentKey);
   revalidatePath(path);
-  return result;
 }
 
-export async function reverseJourneyItemAction(contentKey: string, path: string) {
-  const result = await reverseJourneyItemCompletion(contentKey);
+export async function reverseJourneyItemAction(contentKey: string, path: string): Promise<void> {
+  await reverseJourneyItemCompletion(contentKey);
   revalidatePath(path);
-  return result;
 }
