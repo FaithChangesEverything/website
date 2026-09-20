c460924a6355a3094b15c0bf587ca26f4f288b90
Richard Ball <richiebee47@gmail.com>
2026-09-19 20:12:40 -0500

    Replace Step 4 Journey resources

--- app/journey/resources/stepResourceAssignments.ts
+++ app/journey/resources/stepResourceAssignments.ts
@@ -147,9 +147,12 @@ export const stepResourceAssignments: Record<
 
   "step-4": [
     { id: "song-2026-0014", type: "song", category: "related-song" },
-    { id: "vid-2026-00206", type: "video", category: "related-sermon" },
-    { id: "vid-2026-00207", type: "video", category: "related-sermon" },
-    { id: "vid-2026-00208", type: "video", category: "related-sermon" },
+    { id: "song-2026-0018", type: "song", category: "related-song" },
+    { id: "song-2026-0012", type: "song", category: "related-song" },
+    { id: "ser-2026-0016", type: "sermon", category: "related-sermon" },
+    { id: "ser-2026-0010", type: "sermon", category: "related-sermon" },
+    { id: "ser-2026-0012", type: "sermon", category: "related-sermon" },
+    { id: "ser-2026-0008", type: "sermon", category: "related-sermon" },
     {
       id: "doc-2026-00017",
       type: "document",
@@ -157,11 +160,16 @@ export const stepResourceAssignments: Record<
       label: "Related Bible Study",
     },
     { id: "vid-2026-00182", type: "video", category: "testimony" },
+    { id: "vid-2026-00026", type: "video", category: "related-video" },
+    { id: "vid-2026-00050", type: "video", category: "related-video" },
+    { id: "vid-2026-00056", type: "video", category: "related-video" },
+    { id: "vid-2026-00057", type: "video", category: "related-video" },
+    { id: "vid-2026-00165", type: "video", category: "related-video" },
     {
-      id: "j2h-character-of-god-series",
+      id: "external-tim-mackie-what-is-faith",
       type: "ministry",
       category: "additional-resource",
-      label: "Character of God Bible Study Series",
+      label: "Additional Resources",
     },
     { id: "j2h-prayer-support", type: "prayer", category: "prayer" },
     {
