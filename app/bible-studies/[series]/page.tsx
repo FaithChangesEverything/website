ef26746db3f74daf5e87f46d8f3fb50785851f46
Richard Ball <richiebee47@gmail.com>
2026-09-23 16:08:44 -0500

    Use shared Bible Study card grid

--- app/bible-studies/[series]/page.tsx
+++ app/bible-studies/[series]/page.tsx
@@ -99,7 +99,7 @@ export default async function BibleStudySeriesPage({
               ))}
             </div>
           ) : (
-            <div className={`${styles.studyGrid} ${series.reducedCards ? styles.studyGridReduced : ""}`}>
+            <div className={styles.studyGrid}>
               {series.lessons.map((lesson) => (
                 <BibleStudyLessonCard key={lesson.slug} lesson={lesson} />
               ))}
